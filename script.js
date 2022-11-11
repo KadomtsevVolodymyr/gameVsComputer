33d692ea77079987ea0cc8d84b40569e7d587e0a
Volodymyr <volodymyr.kadomtsev@gmail.com>
2022-11-11 11:11:09 +0100

    game

---
+++ script.js
@@ -0,0 +1,42 @@
+const userName = prompt("Write your name");
+const user__name = document.querySelector(".user__name");
+user__name.textContent = userName;
+const btn = document.querySelector(".generate");
+const userScore = document.querySelector(".user__score");
+const robotScore = document.querySelector(".robot__score");
+const userGenerate = document.querySelector(".user__generate");
+const robotGenerate = document.querySelector(".robot__generate");
+let userScoreNum = 0;
+let robotScoreNum = 0;
+
+const animation = (c) => {
+  c.classList.add("rotate-scale-up");
+  setTimeout(function () {
+    c.classList.remove("rotate-scale-up");
+  }, 750);
+};
+
+btn.addEventListener("click", () => {
+  animation(robotGenerate);
+  animation(userGenerate);
+  let a = Math.floor(Math.random() * 10);
+  let b = Math.floor(Math.random() * 10);
+  userGenerate.textContent = a;
+  robotGenerate.textContent = b;
+  if (a > b) {
+    userScoreNum++;
+    userScore.textContent = userScoreNum;
+  } else if (b > a) {
+    robotScoreNum++;
+    robotScore.textContent = robotScoreNum;
+  }
+  if (userScoreNum === 3) {
+    userScoreNum = 0;
+    robotScoreNum = 0;
+    alert(`${userName} - Win`);
+  } else if (robotScoreNum === 3) {
+    alert("Robot - Win");
+    userScoreNum = 0;
+    robotScoreNum = 0;
+  }
+});
